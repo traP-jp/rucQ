9e19e131c144f9a7b640c14d624c5a0d6a4f4006
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-01-22 19:17:12 +0900

    headerをlayoutに移した

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
 import { RouterView } from 'vue-router'
 import { useDisplay } from 'vuetify'
 import SideBar from '@/components/layout/SideBar.vue'
+import MobileHeader from '@/components/layout/MobileHeader.vue'
 import MobileFooter from '@/components/layout/MobileFooter.vue'
 
 const { mobile } = useDisplay()
@@ -10,6 +11,7 @@ const { mobile } = useDisplay()
 <template>
   <v-app>
     <side-bar v-if=!mobile />
+    <mobile-header v-if="mobile" />
     <v-main>
       <router-view />
     </v-main>
---
+++ frontend/src/components/layout/MobileHeader.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="app-header">
+    <h1 class="app-title">rucQ</h1>
+  </header>
+</template>
+
+
+<style scoped>
+.app-header {
+  display: flex;
+  justify-content: flex-start; /* 左寄せ */
+  align-items: center;
+  background-color: #333; /* ヘッダーの背景色 */
+  color: white; /* 文字色 */
+  padding: 10px 20px; /* 上下左右の余白 */
+  z-index: 100000; /* 他の要素より手前に表示 */
+}
+
+.app-title {
+  font-size: 24px;
+  font-weight: bold;
+  margin: 0;
+}
+</style>
+
