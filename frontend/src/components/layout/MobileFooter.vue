00c8c3cfe7a8b22463fa9474762252a574eb0cdd
mumumu <takumu.sato@gmail.com>
2025-01-25 15:47:09 +0900

    ページ遷移を可能なようにした

--- frontend/src/components/layout/MobileFooter.vue
+++ frontend/src/components/layout/MobileFooter.vue
@@ -1,7 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const router = useRouter()
+const route = useRoute() // 追加
+const value = ref<string>('guidebook')
+
+watch(value, (val: string) => {
+  switch (val) {
+    case 'guidebook':
+      const segments = route.path.split('/')
+      const parentPath = '/' + segments[1] + '/' 
+      router.push(parentPath)
+      break
+    case 'schedule':
+      router.push('schedule')
+      break
+    case 'notes':
+      router.push('notes')
+      break
+    case 'chat':
+      router.push('chat')
+      break
+    case 'info':
+      router.push('info')
+      break
+  }
+})
+</script>
 
 <template>
-  <v-bottom-navigation>
+  <v-bottom-navigation v-model="value" color="primary">
     <v-btn value="guidebook">
       <v-icon>mdi-book-open</v-icon>
     </v-btn>
@@ -18,7 +47,7 @@
       <v-icon>mdi-chat</v-icon>
     </v-btn>
 
-    <v-btn value="information">
+    <v-btn value="info">
       <v-icon>mdi-information</v-icon>
     </v-btn>
   </v-bottom-navigation>
