3ff5bfa32497a66d7038edd850142cf3b4cccd4c
kitsne241 <bicycle.y.t@gmail.com>
2025-01-25 17:15:18 +0900

    合宿選択画面など

---
+++ frontend/src/views/RootView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  // 最新の合宿へリダイレクトする機能をあとで実装
+  router.push({ path: '/camp24_summer' })
+})
+</script>
+
+<template>
+  <div></div>
+</template>
