0881afeb4e736e3fe0ddcc6b082b36d27cacb051
mumumu <takumu.sato@gmail.com>
2025-02-02 01:33:35 +0900

    openapifetchからたたくときにローカル環境で動くように修正

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -2,4 +2,9 @@ import createClient from 'openapi-fetch'
 import type { paths } from '@/api/schema'
 
 const baseUrl = import.meta.env.VITE_BACKEND_ADDR || 'https://rucq.trap.show'
-export const apiClient = createClient<paths>({ baseUrl: baseUrl })
+export const apiClient = createClient<paths>({
+  baseUrl: baseUrl,
+  headers: {
+    'X-Forwarded-User': 'kitsne',
+  },
+})
