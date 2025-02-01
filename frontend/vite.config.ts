2722cb05d1d45488bf8b049fa3f43dc3c3ab8a48
mumumu <takumu.sato@gmail.com>
2025-02-01 14:46:08 +0900

    リクエストの送信先を変更

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,4 +15,18 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    },
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+      protocol: 'ws',
+    }
+  }
 })
