5aefd9a6924f00f5f11b389d8c71fff556df4847
mumumu <takumu.sato@gmail.com>
2025-02-03 10:20:27 +0900

    質問グループの編集用apiを作成

--- backend/handler/api.gen.go
+++ backend/handler/api.gen.go
@@ -351,6 +351,12 @@ type GetQuestionGroupParams struct {
 	XForwardedUser *XForwardedUser `json:"X-Forwarded-User,omitempty"`
 }
 
+// PutQuestionGroupParams defines parameters for PutQuestionGroup.
+type PutQuestionGroupParams struct {
+	// XForwardedUser ログインしているユーザーのtraQ ID（NeoShowcaseが自動で付与）
+	XForwardedUser *XForwardedUser `json:"X-Forwarded-User,omitempty"`
+}
+
 // PostQuestionParams defines parameters for PostQuestion.
 type PostQuestionParams struct {
 	// XForwardedUser ログインしているユーザーのtraQ ID（NeoShowcaseが自動で付与）
@@ -440,6 +446,9 @@ type PutOptionJSONRequestBody = PostOptionRequest
 
 // PostQuestionGroupJSONRequestBody defines body for PostQuestionGroup for application/json ContentType.
 type PostQuestionGroupJSONRequestBody = PostQuestionGroupRequest
+
+// PutQuestionGroupJSONRequestBody defines body for PutQuestionGroup for application/json ContentType.
+type PutQuestionGroupJSONRequestBody = PostQuestionGroupRequest
 
 // PostQuestionJSONRequestBody defines body for PostQuestion for application/json ContentType.
 type PostQuestionJSONRequestBody = PostQuestionRequest
@@ -654,6 +663,9 @@ type ServerInterface interface {
 	// 質問グループの詳細を取得
 	// (GET /api/question_groups/{question_group_id})
 	GetQuestionGroup(ctx echo.Context, questionGroupId QuestionGroupId, params GetQuestionGroupParams) error
+	// 質問グループを更新
+	// (PUT /api/question_groups/{question_group_id})
+	PutQuestionGroup(ctx echo.Context, questionGroupId QuestionGroupId, params PutQuestionGroupParams) error
 	// 質問の一覧を取得
 	// (GET /api/questions)
 	GetQuestions(ctx echo.Context) error
@@ -1310,6 +1322,42 @@ func (w *ServerInterfaceWrapper) GetQuestionGroup(ctx echo.Context) error {
 	return err
 }
 
+// PutQuestionGroup converts echo context to params.
+func (w *ServerInterfaceWrapper) PutQuestionGroup(ctx echo.Context) error {
+	var err error
+	// ------------- Path parameter "question_group_id" -------------
+	var questionGroupId QuestionGroupId
+
+	err = runtime.BindStyledParameterWithOptions("simple", "question_group_id", ctx.Param("question_group_id"), &questionGroupId, runtime.BindStyledParameterOptions{ParamLocation: runtime.ParamLocationPath, Explode: false, Required: true})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Invalid format for parameter question_group_id: %s", err))
+	}
+
+	// Parameter object where we will unmarshal all parameters from the context
+	var params PutQuestionGroupParams
+
+	headers := ctx.Request().Header
+	// ------------- Optional header parameter "X-Forwarded-User" -------------
+	if valueList, found := headers[http.CanonicalHeaderKey("X-Forwarded-User")]; found {
+		var XForwardedUser XForwardedUser
+		n := len(valueList)
+		if n != 1 {
+			return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Expected one value for X-Forwarded-User, got %d", n))
+		}
+
+		err = runtime.BindStyledParameterWithOptions("simple", "X-Forwarded-User", valueList[0], &XForwardedUser, runtime.BindStyledParameterOptions{ParamLocation: runtime.ParamLocationHeader, Explode: false, Required: false})
+		if err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Invalid format for parameter X-Forwarded-User: %s", err))
+		}
+
+		params.XForwardedUser = &XForwardedUser
+	}
+
+	// Invoke the callback with all the unmarshaled arguments
+	err = w.Handler.PutQuestionGroup(ctx, questionGroupId, params)
+	return err
+}
+
 // GetQuestions converts echo context to params.
 func (w *ServerInterfaceWrapper) GetQuestions(ctx echo.Context) error {
 	var err error
@@ -1756,6 +1804,7 @@ func RegisterHandlersWithBaseURL(router EchoRouter, si ServerInterface, baseURL 
 	router.GET(baseURL+"/api/question_groups", wrapper.GetQuestionGroups)
 	router.POST(baseURL+"/api/question_groups", wrapper.PostQuestionGroup)
 	router.GET(baseURL+"/api/question_groups/:question_group_id", wrapper.GetQuestionGroup)
+	router.PUT(baseURL+"/api/question_groups/:question_group_id", wrapper.PutQuestionGroup)
 	router.GET(baseURL+"/api/questions", wrapper.GetQuestions)
 	router.POST(baseURL+"/api/questions", wrapper.PostQuestion)
 	router.DELETE(baseURL+"/api/questions/:question_id", wrapper.DeleteQuestion)
