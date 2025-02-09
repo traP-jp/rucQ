f4e47f48aa155f92cafd3c888547239e9c1f9dae
kitsne <bicycle.y.t@gmail.com>
2025-02-09 18:09:48 +0900

    イベントダイアログにボタンを追加

--- frontend/src/components/EventDialog.vue
+++ frontend/src/components/EventDialog.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-// const emit = defineEmits(['close'])
+const emit = defineEmits(['close'])
 import MarkdownEditor from './MarkdownEditor.vue'
 import { getTimeString } from '@/lib/date'
 
@@ -22,21 +22,60 @@ onMounted(() => {
 </script>
 
 <template>
-  <v-card text-high-emphasis :class="[`bg-${event.display_color}`, $style.card]">
-    <template v-slot:title>
-      <span style="font-weight: 700">{{ event.name }}</span>
-    </template>
-    <template v-slot:subtitle>
-      <span>{{ makeInfo(event) }}</span>
-    </template>
+  <v-card :class="`bg-${event.display_color}`">
+    <div :class="$style.title">
+      <v-card rounded="0" elevation="0" :class="[$style.card, `bg-${event.display_color}`]">
+        <template v-slot:title>
+          <span style="font-weight: 700">{{ event.name }}</span>
+        </template>
+        <template v-slot:subtitle>
+          <span>{{ makeInfo(event) }}</span>
+        </template>
+      </v-card>
+      <div :class="$style.buttons">
+        <v-btn
+          @click="emit('close')"
+          density="comfortable"
+          elevation="0"
+          icon="mdi-close"
+          :baseColor="`${event.display_color}`"
+        ></v-btn>
+        <v-btn
+          density="comfortable"
+          elevation="0"
+          icon="mdi-square-edit-outline"
+          :baseColor="`${event.display_color}`"
+        ></v-btn>
+      </div>
+    </div>
     <MarkdownEditor color="orange" v-if="!isPreview" v-model:text="text" />
+
     <!-- <MarkdownPreview v-else v-model:text="text" />
       <EditPreviewButton :class="$style.button" v-model:isPreview="isPreview" /> -->
   </v-card>
 </template>
 
 <style module>
+.card {
+  height: fit-content;
+  margin: auto 0;
+}
+
 .card :global(.v-card-subtitle) {
   opacity: 1 !important;
+}
+
+.title {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.buttons {
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 6px;
 }
 </style>
