38d72f52b424809205e89dd49b79ed736b8bf484
kitsne <bicycle.y.t@gmail.com>
2025-02-14 18:59:02 +0900

    フッターの i をユーザーアイコンに

--- frontend/src/components/generic/UserIcon.vue
+++ frontend/src/components/generic/UserIcon.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-defineProps<{ id: string }>()
+defineProps<{ id?: string; size?: number }>()
+import { useUserStore } from '@/store'
+const userId = useUserStore().userId
 </script>
 
 <template>
-  <img :src="`https://q.trap.jp/api/v3/public/icon/${id}`" />
+  <img
+    :style="`width: ${size}px; height: ${size}px; object-fit: contain; border-radius: ${(size || 0) / 2}px`"
+    :src="`https://q.trap.jp/api/v3/public/icon/${id || userId}`"
+  />
 </template>
