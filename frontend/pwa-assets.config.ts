c55bf1deac9a29b154e7172b40f459565ca97695
kitsne <bicycle.y.t@gmail.com>
2025-02-19 18:09:43 +0900

    PWA 化

---
+++ frontend/pwa-assets.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from '@vite-pwa/assets-generator/config'
+
+export default defineConfig({
+  images: ['public/icons/icon-512.png'],
+})
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,15 +4,53 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'rucQ',
+        short_name: 'rucQ',
+        description: 'traP の合宿総合アプリ',
+        theme_color: '#ff7300',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
@@ -20,13 +58,13 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
-        changeOrigin: true
-      }
+        changeOrigin: true,
+      },
     },
     hmr: {
       host: 'localhost',
       port: 5173,
       protocol: 'ws',
-    }
-  }
+    },
+  },
 })
