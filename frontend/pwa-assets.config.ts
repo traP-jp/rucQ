56b3d55e06e49dad70cca1323af53dbca7142904
kitsne241 <bicycle.y.t@gmail.com>
2025-02-20 09:05:08 +0900

    アイコンの更新

--- frontend/pwa-assets.config.ts
+++ frontend/pwa-assets.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from '@vite-pwa/assets-generator/config'
 
 export default defineConfig({
-  images: ['public/icons/icon-512.png'],
+  images: ['public/icons/icon.png'],
+  preset: 'minimal-2023',
+  manifestIconsEntry: true,
 })
